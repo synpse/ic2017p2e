04393c2feb2d29b233a7c3171fa9e30f9a69d8e4
Nuno Fachada <nfachada@laseeb.org>
2018-01-26 14:47:33 +0000

    Atualizar nome do projeto

--- code/example.c
+++ code/example.c
@@ -1,19 +1,19 @@
 /*
- * This file is part of "2º Projeto de Introdução à Computação 2017/2018"
- * (2oPIC1718).
+ * This file is part of "Projeto de Introdução à Computação 2ª época 2017/2018"
+ * (PIC2e1718).
  *
- * 2oPIC1718 is free software: you can redistribute it and/or modify
+ * PIC2e1718 is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * 2oPIC1718 is distributed in the hope that it will be useful,
+ * PIC2e1718 is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with 2oPIC1718. If not, see <http://www.gnu.org/licenses/>.
+ * along with PIC2e1718. If not, see <http://www.gnu.org/licenses/>.
  * */
 
 /**
