39a688ca8926545c2c6d71a0412f3915e7fcd41a
Nuno Fachada <nfachada@laseeb.org>
2018-01-26 15:24:21 +0000

    Atualizar código para época especial

--- code/example.c
+++ code/example.c
@@ -39,12 +39,24 @@
 /** Vertical world size. */
 #define WORLD_Y 20
 
+/** Number of human agents. */
+#define NHUMANS 20
+
+/** Number of zombie agents. */
+#define NZOMBIES 20
+
+/** Number of playable human agents. */
+#define NHUMANS_PLAY 1
+
+/** Number of playable zombie agents. */
+#define NZOMBIES_PLAY 0
+
 /**
  * Structure defining agent properties.
  *
  * @note This is an example which will probably not work in a fully functional
- * game. Students should develop their own implementation of
- * ::get_agent_info_at() and agent/world data structures.
+ * game. Students should develop their own implementation of agent in agent.h
+ * and agent.c files, with new/destroy functions.
  * */
 typedef struct {
     AGENT_TYPE type;        /**< Agent type.        */
@@ -52,22 +64,9 @@ typedef struct {
     unsigned short id;      /**< Agent ID.          */
 } AGENT;
 
-/**
- * Structure defining world properties.
- *
- * @note This is an example which will probably not work in a fully functional
- * game. Students should develop their own implementation of
- * ::get_agent_info_at() and agent/world data structures.
- * */
-typedef struct {
-    AGENT *grid;        /**< World is a grid composed of agents. */
-    unsigned int xsize; /**< Horizontal world size.              */
-    unsigned int ysize; /**< Vertical world size.                */
-} WORLD;
-
 /* This function is an implementation of the definition provided by the
- * ::get_agent_info_at() function pointer. It only works for AGENT and WORLD
- * example structures defined in this file. */
+ * ::get_agent_info_at() function pointer. It only works for AGENT example
+ * structure defined in this file. */
 unsigned int example_get_ag_info(void *world, unsigned int x, unsigned int y);
 
 /**
@@ -80,17 +79,16 @@ unsigned int example_get_ag_info(void *world, unsigned int x, unsigned int y);
  * */
 int main() {
 
-    /* An instance of a WORLD structure. */
-    WORLD my_world;
-
     /* An instance of a SHOWWORLD world display. */
     SHOWWORLD *sw = NULL;
 
-    /* A by-dimensional array of agents, representing agents in a grid. */
-    AGENT agent_grid[WORLD_X][WORLD_Y];
-
-    /* Number of agents created so far. */
-    unsigned int nagents = 0;
+    /* A by-dimensional array of agent pointers, representing agents in a
+       grid. In the final project the agent grid or world should be abstracted
+       to specific world.c/world.h files, with new/destroy functions (see files
+       from aula 12).
+       All elements in this grid are initialized to NULL, i.e., they don't
+       initially point to anything. */
+    AGENT *grid[WORLD_X][WORLD_Y] = { NULL };
 
     /* Initialize world display. */
     sw = showworld_new(WORLD_X, WORLD_Y, example_get_ag_info);
@@ -98,61 +96,106 @@ int main() {
     /* Initialize random number generator. */
     srand(time(NULL));
 
-    /* **************************************************************** */
-    /* Cycle through all cells in grid and randomly place agents in it. */
-    /* **************************************************************** */
-    for (int i = 0; i < WORLD_X; ++i) {
-        for (int j = 0; j < WORLD_Y; ++j) {
+    /* ************************************* */
+    /* Create and place human agents in grid */
+    /* ************************************* */
+    for (int i = 0; i < NHUMANS; ++i) {
 
-            /* Possible agent in grid. By default we assume there is none. */
-            AGENT ag = {None, 0, 0};
+        /* Determine random x and y coordinates. */
+        unsigned int x = rand() % WORLD_X;
+        unsigned int y = rand() % WORLD_Y;
 
-            /* Obtain a probability between 0 and 99. */
-            unsigned char probability = rand() % 100;
+        /* Check if there those coordinates are vacant, so we can put a new
+           agent in them. */
+        if (grid[x][y] == NULL) {
 
-            /* There is 10% probability of creating an agent. */
-            if (probability < 10) {
+            /* Is current agent supposed to be playable? */
+            unsigned char playable = (i < NHUMANS_PLAY);
 
-                /* If we got here, an agent will be placed at (i,j). */
+            /* Create new agent and initialize its values. In a real project
+               you should have agent_new() and agent_destroy() functions and
+               not use malloc directly here. */
+            AGENT *ag = malloc(sizeof(AGENT));
+            ag->type = Human;
+            ag->playable = playable;
+            ag->id = (unsigned short) i;
 
-                /* Randomly define agent type. */
-                ag.type = (rand() % 2 == 0) ? Human : Zombie;
+            /* Put agent in grid. In a real project you should probably have
+               a world_put() function or similar (like in Aula 12). */
+            grid[x][y] = ag;
 
-                /* Give 10% probablity of agent being playable by user. */
-                ag.playable = (rand() % 10 == 0);
+        } else {
 
-                /* Assign agent ID and then increment number of agents so
-                   far. */
-                ag.id = nagents++;
-
-            }
-
-            /* Assign possible agent to grid at (i,j). */
-            agent_grid[i][j] = ag;
+            /* If we get here it's because the grid position already has an
+               agent in it. In this case we need to obtain new random (x,y)
+               coordinates, but we also need to decrement the counter so in
+               the end we have the correct number of agents. */
+            i--;
         }
+
     }
 
-    /* ******************************* */
-    /* Populate the my_world variable. */
-    /* ******************************* */
+    /* ********************************************************************* */
+    /* Create and place zombie agents in grid. This for loop is very similar */
+    /* to the previous one for humans, so consider putting this common code  */
+    /* in a function.                                                        */
+    /* ********************************************************************* */
+    for (int i = 0; i < NZOMBIES; ++i) {
 
-    /* A bidimensional array of agents can be interpreted as a pointer to
-       agents. */
-    my_world.grid = (AGENT *) agent_grid;
+        /* Determine random x and y coordinates. */
+        unsigned int x = rand() % WORLD_X;
+        unsigned int y = rand() % WORLD_Y;
 
-    /* World size is defined by constants in this example. */
-    my_world.xsize = WORLD_X;
-    my_world.ysize = WORLD_Y;
+        /* Check if there those coordinates are vacant, so we can put a new
+           agent in them. */
+        if (grid[x][y] == NULL) {
+
+            /* Is current agent supposed to be playable? */
+            unsigned char playable = (i < NZOMBIES_PLAY);
+
+            /* Create new agent and initialize its values. In a real project
+               you should have agent_new() and agent_destroy() functions and
+               not use malloc directly here. */
+            AGENT *ag = malloc(sizeof(AGENT));
+            ag->type = Zombie;
+            ag->playable = playable;
+            ag->id = (unsigned short) i + NHUMANS;
+
+            /* Put agent in grid. In a real project you should probably have
+               a world_put() function or similar (like in Aula 12). */
+            grid[x][y] = ag;
+
+        } else {
+
+            /* If we get here it's because the grid position already has an
+               agent in it. In this case we need to obtain new random (x,y)
+               coordinates, but we also need to decrement the counter so in
+               the end we have the correct number of agents. */
+            i--;
+        }
+
+    }
 
     /* ********************************************************************* */
     /* Show world using the simple_show_world() function. This function can  */
     /* be used in the first part of the project.                             */
     /* ********************************************************************* */
-    showworld_update(sw, &my_world);
+    showworld_update(sw, grid);
 
     /* Before finishing, ask user to press ENTER. */
     printf("Press ENTER to continue...");
     getchar();
+
+    /* Destroy all agents. */
+    for (int i = 0; i < WORLD_X; i++) {
+        for (int j = 0; j < WORLD_Y; j++) {
+            if (grid[i][j] != NULL) {
+                /* In the final project we should have an agent_destroy()
+                   function. */
+                free(grid[i][j]);
+            }
+        }
+    }
 
     /* Destroy world display. */
     showworld_destroy(sw);
@@ -163,12 +206,13 @@ int main() {
 
 /**
  * This function is an implementation of the ::get_agent_info_at() function
- * definition. It only works for ::AGENT and ::WORLD structures defined in this
- * example.
+ * definition. It only works for the ::AGENT structure defined in this example,
+ * and considers the agent world as a 2D grid of agent pointers (as defined in
+ * main).
  *
- * It basically receives a pointer to a ::WORLD structure, obtains the AGENT
- * structure in the given coordinates, and returns the agent information in a
- * bit-packed `unsigned int`.
+ * It basically receives a pointer to a 2D grid of agent pointers, obtains the
+ * AGENT pointer in the given coordinates, and returns the agent information in
+ * a bit-packed `unsigned int`.
  *
  * @note This is an example which will probably not work in a fully functional
  * game. Students should develop their own implementation of
@@ -188,11 +232,12 @@ unsigned int example_get_ag_info(void *w, unsigned int x, unsigned int y) {
     /* The agent information to return. */
     unsigned int ag_info = 0;
 
-    /* Convert generic pointer to world to a WORLD object. */
-    WORLD *my_world = (WORLD *) w;
+    /* Convert generic pointer to world to a 2D grid of agent pointers (in
+       practice we are reinterpreting the grid as 1D). */
+    AGENT **grid = (AGENT **) w;
 
     /* Check if the given (x,y) coordinates are within bounds of the world. */
-    if ((x >= my_world->xsize) || (y >= my_world->ysize)) {
+    if ((x >= WORLD_X) || (y >= WORLD_Y)) {
 
         /* If we got here, then the coordinates are off bounds. As such we will
            report that the requested agent is of unknown type. No need to
@@ -205,10 +250,10 @@ unsigned int example_get_ag_info(void *w, unsigned int x, unsigned int y) {
            agent information. */
 
         /* Obtain agent at specified coordinates. */
-        AGENT ag = my_world->grid[x * my_world->ysize + y];
+        AGENT *ag = grid[x * WORLD_Y + y];
 
         /* Is there an agent at (x,y)? */
-        if (ag.type == None) {
+        if (ag == NULL) {
 
             /* If there is no agent at the (x,y) coordinates, set agent type to
                None. No need to specify agent ID or playable status, since
@@ -220,7 +265,7 @@ unsigned int example_get_ag_info(void *w, unsigned int x, unsigned int y) {
             /* If we get here it's because there is an agent at (x,y). Bit-pack
                all the agent information as specified by the get_agent_info_at
                function pointer definition. */
-            ag_info = (ag.id << 3) | (ag.playable << 2) | ag.type;
+            ag_info = (ag->id << 3) | (ag->playable << 2) | ag->type;
 
         }
 
